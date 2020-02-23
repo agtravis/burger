d558b9cc7d1f6e6b27dc424afeefd0a3dec263ef
George Travis <wewerelikesharks@gmail.com>
2020-02-23 05:17:04 -0800

    update pulls in correct data

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -16,4 +16,32 @@ router.get('/', (req, res) => {
   });
 });
 
+router.put('/api/burgers/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(req.body);
+});
+
+// colToUpdate, newVal, conditionCol, conditionVal
+
+// router.put('/api/cats/:id', (req, res) => {
+//     const condition = 'id = ' + req.params.id;
+
+//     console.log('condition', condition);
+
+//     cat.update(
+//       {
+//         sleepy: req.body.sleepy
+//       },
+//       condition,
+//       function(result) {
+//         if (result.changedRows == 0) {
+//           // If no rows were changed, then the ID must not exist, so 404
+//           return res.status(404).end();
+//         } else {
+//           res.status(200).end();
+//         }
+//       }
+//     );
+//   });
+
 module.exports = router;
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,72 @@
+'use strict';
+
+$(function() {
+  $('.devour-burger').on('click', function() {
+    const id = $(this).data('id');
+    const newDevoured = $(this).data('devoured');
+    const newDevouredState = {
+      devoured: newDevoured
+    };
+    $.ajax(`/api/burgers/${id}`, {
+      type: 'PUT',
+      data: newDevouredState
+    }).then(function() {
+      location.reload();
+    });
+  });
+
+  $('.change-sleep').on('click', function(event) {
+    event.preventDefault();
+    const id = $(this).data('id');
+    const newSleep = $(this).data('newsleep');
+
+    const newSleepState = {
+      sleepy: newSleep
+    };
+
+    // Send the PUT request.
+    $.ajax('/api/cats/' + id, {
+      type: 'PUT',
+      data: newSleepState
+    }).then(function() {
+      console.log('changed sleep to', newSleep);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $('.delete-cat').on('click', function(event) {
+    event.preventDefault();
+    const id = $(this).data('id');
+    $.ajax('/api/cats/' + id, {
+      type: 'DELETE'
+    }).then(function() {
+      console.log('Cat deleted');
+      location.reload();
+    });
+  });
+
+  $('.create-form').on('submit', function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    const newCat = {
+      name: $('#ca')
+        .val()
+        .trim(),
+      sleepy: $('[name=sleepy]:checked')
+        .val()
+        .trim()
+    };
+
+    // Send the POST request.
+    $.ajax('/api/cats', {
+      type: 'POST',
+      data: newCat
+    }).then(function() {
+      console.log('created new cat');
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+});
