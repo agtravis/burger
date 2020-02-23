2898057b324d806995b52d52b4d62cb4ec411f8b
George Travis <wewerelikesharks@gmail.com>
2020-02-23 14:03:51 -0800

    add a new burger works

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -31,19 +31,10 @@ router.put('/api/burgers/:id', (req, res) => {
   });
 });
 
-// cat.update(
-//   {
-//     sleepy: req.body.sleepy
-//   },
-//   condition,
-//   function(result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   }
-// );
+router.post('/api/burgers/', (req, res) => {
+  burger.insertOne('burger_name', req.body.name, function(result) {
+    res.json({ id: result.insertId });
+  });
+});
 
 module.exports = router;
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -15,6 +15,22 @@ $(function() {
     });
   });
 
+  $('.create-form').on('submit', function(event) {
+    event.preventDefault();
+    const newBurger = {
+      name: $('#burg')
+        .val()
+        .trim()
+    };
+    $.ajax('/api/burgers/', {
+      type: 'POST',
+      data: newBurger
+    }).then(function() {
+      console.log('Created new burger!');
+      location.reload();
+    });
+  });
+
   /*
   $('.delete-cat').on('click', function(event) {
     event.preventDefault();
@@ -23,30 +39,6 @@ $(function() {
       type: 'DELETE'
     }).then(function() {
       console.log('Cat deleted');
-      location.reload();
-    });
-  });
-
-  $('.create-form').on('submit', function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
-
-    const newCat = {
-      name: $('#ca')
-        .val()
-        .trim(),
-      sleepy: $('[name=sleepy]:checked')
-        .val()
-        .trim()
-    };
-
-    // Send the POST request.
-    $.ajax('/api/cats', {
-      type: 'POST',
-      data: newCat
-    }).then(function() {
-      console.log('created new cat');
-      // Reload the page to get the updated list
       location.reload();
     });
   });*/
